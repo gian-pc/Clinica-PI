404b2be28677d314180314d849fc7fdb506f7bf0
gianpc <gpaucarcortez@gmail.com>
2024-09-14 20:59:36 -0500

    proyecto integrador

---
+++ src/main/java/com/example/ClinicaOdontologicaSpringMVC/Security/WebConfigSecurity.java
@@ -0,0 +1,54 @@
+package com.example.ClinicaOdontologicaSpringMVC.Security;
+
+import com.example.ClinicaOdontologicaSpringMVC.Service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity //<-- activa la seguridad.
+public class WebConfigSecurity {
+    //esta clase deberia autenticar y autorizar
+    @Autowired
+    private UsuarioService usuarioService;
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+
+    @Bean //el metodo que nos va a ayudar con la autenticacion
+    public DaoAuthenticationProvider daoAuthenticationProvider(){
+        DaoAuthenticationProvider provider= new DaoAuthenticationProvider();
+        provider.setUserDetailsService(usuarioService);
+        provider.setPasswordEncoder(bCryptPasswordEncoder);
+        return provider;
+    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+
+                .authorizeHttpRequests((authz)-> authz
+                        .requestMatchers("/index.html").permitAll()
+                        //.requestMatchers("/post_odontologos.html").hasRole("USER")
+                        .requestMatchers("/post_odontologos.html", "/post_pacientes.html").hasRole("ADMIN")
+                        .requestMatchers("/post_turnos.html").hasRole("USER")
+                        .anyRequest().authenticated())
+                .formLogin(withDefaults()).logout(withDefaults());
+
+
+
+        return http.build();
+
+    }
+
+}
